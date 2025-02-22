7662e3947f7dd82e9caeb2951f42dfcec1ae5246
Moamen Hassaballah <hassaballahmoamen84@gmail.com>
2025-02-23 01:16:02 +0200

    Initial Commit

---
+++ Onboarding Screen/Onboarding_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_ScreenApp.swift
+//  Onboarding Screen
+//
+//  Created by Moamen Hassaballah on 23/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Onboarding ScreenTests/Onboarding_ScreenTests.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_ScreenTests.swift
+//  Onboarding ScreenTests
+//
+//  Created by Moamen Hassaballah on 23/02/2025.
+//
+
+import Testing
+@testable import Onboarding_Screen
+
+struct Onboarding_ScreenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
